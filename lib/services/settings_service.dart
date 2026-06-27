a1eddda539a0050baac716fd4c42184cb814b5e0
Thomas Arnaud <38663853+Nohzoh@users.noreply.github.com>
2026-06-27 17:29:42 +0200

    feat: simplify SettingsService for Gemini API key

--- lib/services/settings_service.dart
+++ lib/services/settings_service.dart
@@ -1,58 +1,32 @@
 import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import '../models/app_settings.dart';
 
 class SettingsService extends ChangeNotifier {
   late SharedPreferences _prefs;
-
   bool _isOnboardingComplete = false;
-  AIMode _aiMode = AIMode.cloud;
-  String? _selectedModelId;
-  String? _selectedProviderId;
-  Map<String, String> _apiKeys = {};
+  String _geminiApiKey = '';
 
   bool get isOnboardingComplete => _isOnboardingComplete;
-  AIMode get aiMode => _aiMode;
-  String? get selectedModelId => _selectedModelId;
-  Map<String, String> get apiKeys => _apiKeys;
+  String get geminiApiKey => _geminiApiKey;
 
   Future<void> init() async {
     _prefs = await SharedPreferences.getInstance();
     _isOnboardingComplete = _prefs.getBool('onboarding_complete') ?? false;
-    _aiMode = AIMode.values[_prefs.getInt('ai_mode') ?? 0];
-    _selectedModelId = _prefs.getString('selected_model');
-    _selectedProviderId = _prefs.getString('selected_provider');
-    final keys = _prefs.getStringList('api_key_names') ?? [];
-    for (final key in keys) {
-      _apiKeys[key] = _prefs.getString('api_key_$key') ?? '';
-    }
+    _geminiApiKey = _prefs.getString('gemini_api_key') ?? '';
   }
 
-  Future<void> completeOnboarding({
-    required AIMode mode,
-    String? modelId,
-    Map<String, String>? apiKeys,
-  }) async {
-    _aiMode = mode;
-    _selectedModelId = modelId;
-    if (apiKeys != null) _apiKeys = apiKeys;
+  Future<void> completeOnboarding({required String apiKey}) async {
+    _geminiApiKey = apiKey;
     _isOnboardingComplete = true;
-
-    await _prefs.setInt('ai_mode', mode.index);
+    await _prefs.setString('gemini_api_key', apiKey);
     await _prefs.setBool('onboarding_complete', true);
-    if (modelId != null) await _prefs.setString('selected_model', modelId);
-    if (apiKeys != null) {
-      await _prefs.setStringList('api_key_names', apiKeys.keys.toList());
-      for (final entry in apiKeys.entries) {
-        await _prefs.setString('api_key_${entry.key}', entry.value);
-      }
-    }
     notifyListeners();
   }
 
   Future<void> resetOnboarding() async {
     await _prefs.clear();
     _isOnboardingComplete = false;
+    _geminiApiKey = '';
     notifyListeners();
   }
 }
